88e08cac4a6f2c55bcebbb25916a8abfc32824d1
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-08-31 00:03:48 +0900

    add test

--- t.py
+++ t.py
@@ -1,7 +1,42 @@
-for i in range(1, 21):
-    if i % 3 == 0:
-        print "Fizz"
-    elif i % 5 == 0:
-        print "Buzz"
-    else:
-        print i
+"""
+>>> main()
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+Fizz
+16
+17
+Fizz
+19
+Buzz
+"""
+
+
+def main():
+    for i in range(1, 21):
+        if i % 3 == 0:
+            print "Fizz"
+        elif i % 5 == 0:
+            print "Buzz"
+        else:
+            print i
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+
+if __name__ == "__main__":
+    _test()
